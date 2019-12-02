6e95250096d23c5587f956a0bd4027d22dc80be9
Gasperrini <56118906+Gasperrini@users.noreply.github.com>
2019-12-02 11:29:16 +0200

    testu atlikimas baigtas

--- test_calc.php
+++ test_calc.php
@@ -2,62 +2,86 @@
 // operacija3.php  Parodoma registruotų vartotojų lentelė
 
 session_start();
-if (!isset($_SESSION['prev']) || ($_SESSION['prev'] != "index"))
-{ header("Location: logout.php");exit;}
+if (!isset($_SESSION['prev']) || ($_SESSION['prev'] != "index")) {
+    header("Location: logout.php");
+    exit;
+}
 
 ?>
 <html>
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8">
-        <title>sdg</title>
-        <link href="include/styles.css" rel="stylesheet" type="text/css" >
-    </head>
-    <body>
-        <table class="center" ><tr><td>
-            <center><img src="include/top.png"></center>
-        </td></tr><tr><td> 
- <?php
-		include("include/meniu.php"); //įterpiamas meniu pagal vartotojo rolę
- ?> 
-        <center><font size="5"><?php $title?></font></center><br>
 
-<?php
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8">
+    <title>sdg</title>
+    <link href="include/styles.css" rel="stylesheet" type="text/css">
+</head>
 
-$connection = mysqli_connect("localhost", "root", "", "vartvald");
-if ($connection->connect_error) {
-    die("Connection failed:" . $connection->connect_error);
-}
+<body>
+    <table class="center">
+        <tr>
+            <td>
+                <center><img src="include/top.png"></center>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php
+                include("include/meniu.php"); //įterpiamas meniu pagal vartotojo rolę
+                ?>
+                <center>
+                    <font size="5"><?php $title ?></font>
+                </center><br>
 
-if (isset($_POST['fk'])) {
-    $fk = mysqli_real_escape_string($connection, $_POST['fk']);
-}
+                <?php
 
-if (isset($_POST['id'])) {
-    $id = mysqli_real_escape_string($connection, $_POST['id']);
-}
+                $connection = mysqli_connect("localhost", "root", "", "vartvald");
+                if ($connection->connect_error) {
+                    die("Connection failed:" . $connection->connect_error);
+                }
 
-if (isset($_POST['mark'])) {
-    $mark = ($_POST['mark']);
-}
+                if (isset($_POST['fk'])) {
+                    $fk = mysqli_real_escape_string($connection, $_POST['fk']);
+                }
 
-if (isset($_POST['correct'])) {
-    $correct = ($_POST['correct']);
-}
+                if (isset($_POST['id'])) {
+                    $id = mysqli_real_escape_string($connection, $_POST['id']);
+                }
 
-$grade = 0;
+                if (isset($_POST['mark'])) {
+                    $mark = ($_POST['mark']);
+                }
 
-for($id = 0; $id < 3; $id++){
-if($_POST['answer_'.$id] == $correct[$id])
-{
-    $grade = $grade + $mark[$id];
-}
-}
+                if (isset($_POST['correct'])) {
+                    $correct = ($_POST['correct']);
+                }
 
-/*$sql = "INSERT INTO test_attempts (date, id, mark, fk_user, fk_test) VALUES
-        ('current_timestamp()','','$grade','$postal_code', '$id') ";*/
+                if (isset($_POST['count'])) {
+                    $count = ($_POST['count']);
+                }
 
-echo "<h2 style='text-align:center'>Surinkai ".$grade." is 4</h2>";
+                $top_grade = 0;
 
-$connection->close();
-?>
-</body></html>
+                $grade = 0;
+
+                for ($id = 0; $id < $count; $id++) {
+                    if ($_POST['answer_' . $id] == $correct[$id]) {
+                        $grade = $grade + $mark[$id];
+                    }
+                    $top_grade = $top_grade + $mark[$id];
+                }
+
+                $user=$_SESSION['user'];
+                $userid=$_SESSION['userid'];
+
+                $sql = "INSERT INTO test_attempts (date, id, mark, top_mark, fk_user, fk_test) VALUES
+                    (CURRENT_TIMESTAMP,null,'$grade','$top_grade','$userid','$fk');";
+
+                mysqli_query($connection,$sql);
+
+                echo "<h2 style='text-align:center'>Surinkai " . $grade . " is " . $top_grade . "!</h2>";
+
+                $connection->close();
+                ?>
+</body>
+
+</html>
